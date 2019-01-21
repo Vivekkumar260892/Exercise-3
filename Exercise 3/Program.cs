4fb319cba8cac4a4dff4fab145156406a024fcdb
Vivekkumar260892 <vivekkumar1@mail.usf.edu>
2019-01-21 16:41:16 -0500

    Exercise 3

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -6,7 +6,30 @@ namespace Exercise_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter A : ");
+            
+            String Number_1= Console.ReadLine();
+            int a = int.Parse(Number_1);
+            Console.WriteLine("Enter B : ");
+            String Number_2 = Console.ReadLine();
+            int b = int.Parse(Number_2);
+            Console.WriteLine("Enter C : ");
+            String Number_3 = Console.ReadLine();
+            int c = int.Parse(Number_3);
+
+            if (a > b && b > c)
+            {
+                Console.WriteLine("A is the greatest number ");
+                if (b > a && b > c)
+                {
+                    Console.WriteLine("B is the greatest number ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("C is the greatest number ");
+            }
+
         }
     }
 }
